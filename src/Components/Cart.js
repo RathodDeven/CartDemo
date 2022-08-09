dfeb259e6b89d56c2733226130d5a6038e4cbdbd
RathodDeven <devenrathodrd@gmail.com>
2022-08-09 17:58:30 +0530

    up

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -4,6 +4,7 @@ import ProductCard from './ProductCard';
 const API_ENDPOINT = 'http://localhost:8000/api/cart/productsFromCartId?'
 const qs = new URLSearchParams(window.location.search);
 console.log(window.location.search)
+const clientId = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MWZiZTJhY2JmMjk2ZDJlMmE4YTExMjMiLCJjbGllbnRQdWJsaWNLZXkiOiIxWE1nR1NzVjRFIiwiaWF0IjoxNjU5NzkxOTE2fQ.xfUEVXqcn2B8tEx-g4_R1rcGumRhdQkEZMgFOsOBkX8"
 
 const Cart = () => {
   //get search query from url
@@ -13,7 +14,13 @@ const Cart = () => {
 
   const fetchProductsInfoFromCartId = async(cartId) => {
     
-    return await fetch(API_ENDPOINT + new URLSearchParams({cartId}))
+    return await fetch(API_ENDPOINT + new URLSearchParams({cartId}), {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': clientId
+      }
+    })
       .then(response => {
         console.log(response)
 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div style={{display: "flex",flexDirection: "column"}}>
+      Home
+      {/* <iframe src="https://interality.io/vn4uTjw/memeverse" style={{width: "100%", height: "100vh"}} allow="microphone; camera; vr; speaker;"></iframe> */}
+      {/* <iframe src="https://youtube.com" style={{width: "1024px", height: "720px"}} allow="microphone; camera; vr; speaker;"></iframe> */}
+      {/* <iframe src="https://hubs.mozilla.com/CA4K8SG?embed_token=null" style={{width: "1024px", height: "768px"}} allow="microphone; camera; vr; speaker;"></iframe> */}
+      <iframe src="https://interality.io" style={{width: "1024px", height: "768px"}} allow="microphone; camera; vr; speaker;"></iframe>
+      
+      </div>
   )
 }
 
